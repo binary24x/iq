2adbcbf6e0df8de8fa9e3f7b6d7d75940e2a47f4
binary24 <binaryoptionclass@gmail.com>
2019-10-23 20:02:24 +0530

    Initial commit

---
+++ run.py
@@ -0,0 +1,24 @@
+from iqoptionapi.stable_api import IQ_Option
+import logging
+import random
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
+I_want_money=IQ_Option("maduabeyaa@gmail.com","19970218")
+ALL_Asset=I_want_money.get_all_open_time()
+#check if open or not
+print(ALL_Asset["forex"]["EURUSD"]["open"])
+print(ALL_Asset["cfd"]["FACEBOOK"]["open"])#Stock,Commodities,ETFs
+print(ALL_Asset["crypto"]["BTCUSD-L"]["open"])
+print(ALL_Asset["digital"]["EURUSD-OTC"]["open"])
+
+#Binary have two diffenence type:"turbo","binary"
+print(ALL_Asset["turbo"]["EURUSD-OTC"]["open"])
+print(ALL_Asset["binary"]["EURUSD-OTC"]["open"])
+
+
+#!!!! exception ""
+print(ALL_Asset["binary"]["not exist asset"]["open"])#it will return "{}" a None of the dict
+
+#!!!!print all!!!!
+for type_name, data in ALL_Asset.items():
+    for Asset,value in data.items():
+        print(type_name,Asset,value["open"])
